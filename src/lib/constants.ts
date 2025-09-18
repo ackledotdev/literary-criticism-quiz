0d91f58bf953c121a7f8b5cb2eed800eea7fe073
Akhil Pillai <ackledotdev@gmail.com>
2025-09-18 08:07:36 -0700

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const Domain = 'schoolsofcriticismquiz.vercel.app';
 export const RootUrl = `https://${Domain}`;
 
-export const PersonalDomain = 'schoolsofcriticism.quiz.ackle.dev';
+export const PersonalDomain = 'literarycriticism.quiz.ackle.dev';
 export const PersonalRootUrl = `https://${PersonalDomain}`;
